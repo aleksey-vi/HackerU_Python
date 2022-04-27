dd461cdffbe2c2647b6c0d81f3de7ba4199c045e
aleksey-vi <65017000+aleksey-vi@users.noreply.github.com>
2022-04-28 01:57:23 +0300

    сортировка чисел

--- dz_Lesson_1.1.py
+++ dz_Lesson_1.1.py
@@ -2,7 +2,7 @@
 
 a = int(input("Введите число 1: "))
 b = int(input("Введите число 2: "))
-
+ 
 my_list = [a, b]
 my_list.sort(reverse=True) # True- сортирует в порядке убывания False-в порядке возрастания
 print(my_list)
